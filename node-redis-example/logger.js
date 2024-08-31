c45af8fcd63e92a16d2ea90ed8e14ef5610cbf23
Max Hniebergall <max@hniebergall.com>
2024-08-31 12:34:09 -0400

    basic redis with express server implementation

---
+++ node-redis-example/logger.js
@@ -0,0 +1,25 @@
+import { createLogger, format, transports } from 'winston';
+
+export function newLogger(name) {
+        return createLogger({
+        level: "info",
+        format: format.combine(
+            format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            format.errors({ stack: true }),
+            format.splat(),
+            format.json()
+        ),
+        defaultMeta: { service: name }, 
+        transports: [
+            new transports.Console({
+                format: format.combine(
+                    format.colorize(),
+                    format.simple()
+                )
+            }),
+            new transports.File({ filename: 'error.log', level: 'error' }),
+            new transports.File({ filename: 'combined.log' })
+        ]
+    });
+}
+
