d03ba61224d8c962abc00e299d7cf93a8ef1bce1
Max Hniebergall <max@hniebergall.com>
2025-02-24 12:09:12 +0100

    update quote utils to use SHA256 hashing

--- backend/utils/quoteUtils.ts
+++ backend/utils/quoteUtils.ts
@@ -1,12 +1,12 @@
 import { Quote } from "../types/index.js";
+import { createHash } from 'crypto';
+
+
 
 export function getQuoteKey(quote: Quote): string {
     if (!quote.sourcePostId || !quote.selectionRange) {
         throw new Error("Invalid quote object: missing sourcePostId or selectionRange.");
     }
-
-    // TODO we should use a fixed size hash function to generate a key
-    // we should look into which hash function is best for our use case
-    // it doesn't need to be secure, we can use a simple hash function
-    return btoa(`${quote.sourcePostId}|${quote.text}|${quote.selectionRange.start}-${quote.selectionRange.end}`);
-} 
+    const input = `${quote.text}|${quote.sourcePostId}|${quote.selectionRange.start}-${quote.selectionRange.end}`;
+    return `quotes:${createHash('sha256').update(input).digest('base64')}`;
+}
